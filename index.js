c085428d8b5a3e5bc9a4bd92cbb1e36c1c1c5e6f
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-11-15 11:00:43 -0800

    as

--- index.js
+++
@@ -1,5 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App.tsx';
-
-// This line tells Expo to start your app with the component exported from App.tsx
-registerRootComponent(App);
