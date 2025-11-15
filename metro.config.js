14ad093f719e5eadcc9c2031425a9c9f29da4cec
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-11-14 19:21:04 -0800

    chore: enhance iOS build workflow by verifying npm ci compatibility and updating dependency installation command

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,10 @@
+const { getDefaultConfig } = require('expo/metro-config');
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-const config = getSentryExpoConfig(__dirname);
+// Get the default Expo config first
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Then apply Sentry config on top of it
+const config = getSentryExpoConfig(__dirname, defaultConfig);
 
 module.exports = config;
