d2d920dfa153702667465ef4abf82872872530f8
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2026-01-04 12:03:04 -0800

    feat: Update socket.io and socket.io-client to version 4.8.3; refactor server setup in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,12 @@ import cors from 'cors';
 import apiRouter from './routes/index';
 import config from './config/serverConfig';
 import type { ApiResponse } from './types';
+import {createServer} from 'node:http';
+import { Server } from 'socket.io';
 
 const app: Express = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +24,16 @@ app.get('/ping', (_req, res) => {
   return res.json(response);
 });
 
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+}
+
+
 const PORT = config.PORT;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
